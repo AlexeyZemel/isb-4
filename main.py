c4df47308ca5af8d3c995bcf382798eef12af59e
Alexey Zemel <loyxa.zemel@gmail.com>
2023-05-16 18:25:40 +0400

    added the luhn alg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #4006234246b4fd2b2833d740927ab20465afad862c74b1a88ec0869bde5c836c|Mastercard|Дебетовая|Альфа-банк|0254|sha256|
 import hashlib
 import tqdm 
+import json
 import logging
 import multiprocessing as mp 
 
@@ -76,3 +77,70 @@ def write_file(data: str, file_name: str) -> None:
     except OSError as err:
         logging.info("Write the data is failed")
         raise err
+    
+
+def read_list(file_name: str) -> list:
+    """Read the data in the file and writes it into list
+
+    Args: 
+        file_name(str): name of the file
+
+    Returns:
+        list: list with the read data
+    """
+    data = []
+    try:
+        with open(file_name, 'r') as f:
+            data = f.readlines()
+            data = list(map(int, data))
+        logging.info("Read the data successfuly")
+    except OSError as err:
+        logging.info("Read the data is failed")
+        raise err
+    return data
+
+
+def load_settings(json_file: str) -> dict:
+    """Loads a settings file into the program.
+
+    Args:
+        json_file (str): The path to the json file with the settings.
+
+    Returns:
+        dict: dictionary with settings
+    """
+    settings = None
+    try:
+        with open(json_file) as json_file:
+            settings = json.load(json_file)
+        logging.info("Settings loaded successfully")
+    except OSError as err:
+        logging.info("Settings not loaded")
+        raise err
+    return settings
+
+
+def luhn(card_number: str) -> bool:
+    """Checking the card number using the Luhn algorithm
+
+    Args: 
+        card_number(srt): number of the card 
+
+    Returns: 
+        bool: is the card number real
+    """
+    nums = []
+    card = list(map(int, card_number))
+    last = card[15]
+    card.pop()
+    for num in card: 
+        tmp = num*2
+        if tmp>9:
+            nums.append(tmp%10 + tmp//10)
+        else:
+            nums.append(tmp)
+    res = 0
+    for num in nums:
+        res += num
+    res = 10 - res % 10
+    return res == last
