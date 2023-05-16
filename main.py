7c0ed6d689cd99e82684a48e624c2f902eeb97d2
Alexey Zemel <loyxa.zemel@gmail.com>
2023-05-16 18:57:38 +0400

    added functions for stat

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#4006234246b4fd2b2833d740927ab20465afad862c74b1a88ec0869bde5c836c|Mastercard|Дебетовая|Альфа-банк|0254|sha256|
+# 4006234246b4fd2b2833d740927ab20465afad862c74b1a88ec0869bde5c836c|Mastercard|Дебетовая|Альфа-банк|0254|sha256|
 import hashlib
-import tqdm 
+import tqdm
 import json
 import logging
-import multiprocessing as mp 
+import csv
+import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 
 def check_hash(hash: str, card_number: str) -> bool:
@@ -20,7 +22,9 @@ def check_hash(hash: str, card_number: str) -> bool:
     return hashlib.sha256(card_number.encode()).hexdigest() == hash
 
 
-def get_card_number(hash: str, bins: list, last_numbs: str, core_number: int = mp.cpu_count()) -> int:
+def get_card_number(
+    hash: str, bins: list, last_numbs: str, core_number: int = mp.cpu_count()
+) -> int:
     """Selects the card number
 
     Args:
@@ -29,7 +33,7 @@ def get_card_number(hash: str, bins: list, last_numbs: str, core_number: int = m
         last_numbs (str): last 4 numbers of the card
         core_number (int): number of processor cores
 
-    Returns: 
+    Returns:
         int: card number or 0 if failed
     """
     args = []
@@ -49,12 +53,12 @@ def read_file(file_name: str) -> str:
 
     Args:
         file_name(str): name of the file
-    
+
     Returns:
         str: data in the file
     """
     try:
-        with open(file_name, 'r') as f:
+        with open(file_name, "r") as f:
             data = f.read()
         logging.info("Read the data successfully")
     except OSError as err:
@@ -71,18 +75,18 @@ def write_file(data: str, file_name: str) -> None:
         file_name(str): name of the file where to write
     """
     try:
-        with open(file_name, 'w') as f:
+        with open(file_name, "w") as f:
             f.write(data)
         logging.info("Write the data successfully")
     except OSError as err:
         logging.info("Write the data is failed")
         raise err
-    
+
 
 def read_list(file_name: str) -> list:
     """Read the data in the file and writes it into list
 
-    Args: 
+    Args:
         file_name(str): name of the file
 
     Returns:
@@ -90,7 +94,7 @@ def read_list(file_name: str) -> list:
     """
     data = []
     try:
-        with open(file_name, 'r') as f:
+        with open(file_name, "r") as f:
             data = f.readlines()
             data = list(map(int, data))
         logging.info("Read the data successfuly")
@@ -123,24 +127,85 @@ def load_settings(json_file: str) -> dict:
 def luhn(card_number: str) -> bool:
     """Checking the card number using the Luhn algorithm
 
-    Args: 
-        card_number(srt): number of the card 
+    Args:
+        card_number(srt): number of the card
 
-    Returns: 
+    Returns:
         bool: is the card number real
     """
     nums = []
     card = list(map(int, card_number))
     last = card[15]
     card.pop()
-    for num in card: 
-        tmp = num*2
-        if tmp>9:
-            nums.append(tmp%10 + tmp//10)
+    for num in card:
+        tmp = num * 2
+        if tmp > 9:
+            nums.append(tmp % 10 + tmp // 10)
         else:
             nums.append(tmp)
     res = 0
     for num in nums:
         res += num
     res = 10 - res % 10
+    logging.info("Check the card number using the Luhn algorithm")
     return res == last
+
+
+def create_stats(statistics: dict, file_name: str) -> None:
+    """Draws and saves the plot based on the statistic
+
+    Args:
+        statistic(dict): data for the plot
+        file_name(str): path for saving
+    """
+    fig = plt.figure(figsize=(30, 5))
+    plt.ylabel("Time")
+    plt.xlabel("Processes")
+    plt.title("Stats")
+    x = statistics.keys()
+    y = statistics.values()
+    plt.bar(x, y, color="blue", width=0.5)
+    plt.savefig(file_name)
+    logging.info("Create and save statistics")
+
+
+def write_stats(processes: int, time: float, file_name: str) -> None:
+    """Writes statistic to the file 
+
+    Args: 
+        processes(int): number of processes
+        time(float): executing time
+        file_name(str): name of the file 
+    """
+    try:
+        with open(file_name, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([processes, time])
+        logging.info("Stats successfully written")
+    except OSError as err:
+        logging.info("Stats writing is failed")
+        raise err
+
+
+def load_stats(file_name: str) -> dict:
+    """Loads statistics from the file 
+
+    Args: 
+        file_name(str): name of the file 
+    
+    Returns: 
+        dict: dict with statistics from the file
+    """
+    try:
+        with open(file_name, 'r') as f:
+            reader = csv.reader(f)
+            stats = list(reader)
+        logging.info("Stats successfully loaded")
+    except OSError as err:
+        logging.info("Loading stats is failed")
+        raise err
+    result = dict()
+    for i in stats:
+        processes, time = i
+        result[int(processes)] = float(time)
+    return result
